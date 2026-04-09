9150ce1bdda6ce48c483edb2e7915f9359a7f61a
youxuhu <huyouxu123@outlook.com>
2026-04-09 23:39:55 +0800

    添加pixel-icom

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 // 添加Element Plus UI库的引入
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import ElementPlus from 'element-plus'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
-
 
 import App from './App.vue'
 import router from './router'
+// 添加Pixel UI库的引入
+import PixelUI from '@mmt817/pixel-ui'
+import '@mmt817/pixel-ui/dist/index.css'
+
 
 const app = createApp(App)
 
@@ -19,5 +19,6 @@ app.use(router)
 app.use(ElementPlus, {
   locale: zhCn,
 })
+app.use(PixelUI)
 
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <px-button @click="handleOpen">Button</px-button>
 </template>

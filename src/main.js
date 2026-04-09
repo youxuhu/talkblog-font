238eec9a4667fdbcab4cfbdb2c1d96540ca744e3
youxuhu <huyouxu123@outlook.com>
2026-04-09 22:53:41 +0800

    添加Element-plus

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+// 添加Element Plus UI库的引入
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +16,8 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(ElementPlus, {
+  locale: zhCn,
+})
 
 app.mount('#app')
